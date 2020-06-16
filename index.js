323ba6a2657a608919e5d2e9c2a59f17461d5853
Daniel <daniel.cassiano@live.com>
2020-06-16 16:45:43 -0300

    CRUD usando Imsomnia e Express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
 //Query = ?nome=1
 /*server.get('/rota/', (req, res) =>{
@@ -19,6 +20,7 @@ const server = express();
 
 //Request body = {"name": Daniel, "e-mail": daniel.cassiano@gmail.com}
 
+/*
 const users = ['Daniel0', 'Cassiano1', 'Daniel2']; //INDEXs: [0,1,2]
 
 server.get('/usuarios/:index', (req, res) =>{
@@ -26,5 +28,40 @@ server.get('/usuarios/:index', (req, res) =>{
 
   return res.json({message: `Array Index Number: ${users[index]}`});
 });
+*/
+
+const users = ['Daniel0', 'Cassiano1', 'Daniel2']; //INDEXs: [0,1,2]
+
+server.get('/users', (req,res) => { //Retorne todos usuários
+    return res.json(users);
+});
+
+server.get('/users/:index', (req,res) => { //Retorne o slot do array index
+    const { index } = req.params;
+    return res.json(users[index]); 
+});
+
+server.post('/users', (req,res) => { //Pegue a variável "name", da requisição body (req.body)
+    const { name } = req.body;
+    users.push(name);
+    return res.json(users);
+});
+
+server.put('/users/:index', (req,res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => { //Apague o último elemento do array
+    const { index } = req.params;
+    users.splice(index, 1); //delete um slot no array index
+    return res.json(users);
+});
+
+
 
 server.listen(3000);
