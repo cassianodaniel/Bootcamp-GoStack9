727b64e314c719f1b9621a65e7b2f29b6e8eb930
Daniel <daniel.cassiano@live.com>
2020-07-01 16:17:53 -0300

    Middleware de Autenticação JWT-PROMISIFY

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -4,6 +4,8 @@ import User from './app/models/User';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 /*
@@ -19,7 +21,11 @@ const user = await User.create({
 */
 
 routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
 
 export default routes;
